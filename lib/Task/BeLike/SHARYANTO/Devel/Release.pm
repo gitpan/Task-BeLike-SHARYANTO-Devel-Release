6d12579bd84c57a87958fdba107f99932775aa95
Steven Haryanto <stevenharyanto@gmail.com>
2013-11-08 03:50:54 +0000

    Import of SHARYANTO/Task-BeLike-SHARYANTO-Devel-Release-0.01 from CPAN.
    gitpan-cpan-distribution: Task-BeLike-SHARYANTO-Devel-Release
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         SHARYANTO/Task-BeLike-SHARYANTO-Devel-Release-0.01.tar.gz
    gitpan-cpan-author:       SHARYANTO
    gitpan-cpan-maturity:     released

---
+++ lib/Task/BeLike/SHARYANTO/Devel/Release.pm
@@ -0,0 +1,69 @@
+package Task::BeLike::SHARYANTO::Devel::Release;
+
+our $VERSION = '0.01'; # VERSION
+
+1;
+# ABSTRACT: Install modules used when SHARYANTO needs to release a Perl distribution
+
+__END__
+
+=pod
+
+=encoding utf-8
+
+=head1 NAME
+
+Task::BeLike::SHARYANTO::Devel::Release - Install modules used when SHARYANTO needs to release a Perl distribution
+
+=head1 VERSION
+
+version 0.01
+
+=head1 DESCRIPTION
+
+This task is currently used by me to update my L<Dist::Zilla> and L<Pod::Weaver>
+plugins prior to releasing.
+
+=head1 FUNCTIONS
+
+
+None are exported by default, but they are exportable.
+
+=head1 TASK CONTENTS
+
+=head2 Included modules
+
+=head3 L<Dist::Zilla::PluginBundle::Author::SHARYANTO>
+
+=head3 L<Pod::Weaver::PluginBundle::Author::SHARYANTO>
+
+=head1 HOMEPAGE
+
+Please visit the project's homepage at L<https://metacpan.org/release/Task-BeLike-SHARYANTO-Devel-Release>.
+
+=head1 SOURCE
+
+Source repository is at L<HASH(0x2add680)>.
+
+=head1 BUGS
+
+Please report any bugs or feature requests on the bugtracker website
+L<https://rt.cpan.org/Public/Dist/Display.html?Name=Task-BeLike-SHARYANTO-D
+evel-Release>
+
+When submitting a bug or request, please include a test-file or a
+patch to an existing test-file that illustrates the bug or desired
+feature.
+
+=head1 AUTHOR
+
+Steven Haryanto <stevenharyanto@gmail.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2013 by Steven Haryanto.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
